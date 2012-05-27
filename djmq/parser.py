1b45c7046bdf9f7d5872eada9d37df51a4fdea92
James Socol <james@mozilla.com>
2012-05-27 19:46:28 -0400

    Serious progress.

--- djmq/parser.py
+++ djmq/parser.py
@@ -6,7 +6,7 @@ except ImportError:
     from _query_utils import Q
 
 
-def parse_query(query):
+def parse_query(query, o=None):
     """Turns a MongoDB-style query dict into a Q object."""
     q = []
     iterator = getattr(query, 'items', query)
@@ -18,9 +18,25 @@ def parse_query(query):
         else:
             k, v = x[0], x[1]
         if k == '$or':
-            or_ = parse_query(v)
+            or_ = parse_query(v, operator.or_)
             q.append(or_)
+        elif k == '$not':
+            not_ = parse_query(v)
+            not_.negate()
+            q.append(not_)
         else:
-            q.append(Q((k, v)))
+            if hasattr(x, 'keys') and len(x.keys()) > 1:
+                q.append(parse_query(x))
+            else:
+                if isinstance(v, dict):
+                    for comp, value in v.items():
+                        if comp == '$gt':
+                            q.append(Q(**{'%s__gt' % k: value}))
+                        elif comp == '$lt':
+                            q.append(Q(**{'%s__lt' % k: value}))
+                else:
+                    q.append(Q((k, v)))
 
-    return reduce(operator.and_, q)
+    if q:
+        return reduce(o or operator.and_, q)
+    return Q()
--- djmq/tests.py
+++ djmq/tests.py
@@ -1,47 +1,90 @@
-from nose.tools import eq_
-
 try:
     from django.db.query_utils import Q
 except ImportError:
     from djmq._query_utils import Q
 
+from djmq import _query_utils
+
+
+def eq_(a, b, msg=None):
+    message = msg or '%s != %s' % (a, b)
+    assert a == b, message
+
+
+if not hasattr(Q, '__eq__'):
+    Q.__eq__ = _query_utils.__eq__
+    Q.__ne__ = _query_utils.__ne__
+
+
 from djmq.parser import parse_query
 
 
-def fix_children(l):
-    for i, x in enumerate(l):
-        if isinstance(x, dict):
-            l[i] = x.items()[0]
-
-
-def compare_query(a, b, msg=None):
-    """Compare two Q objects, since Q.__eq__ doesn't exist."""
-    ad, bd = a.__dict__, b.__dict__
-    fix_children(ad['children'])
-    fix_children(bd['children'])
-    eq_(ad, bd, msg or '%r != %r' % (a, b))
+def test_empty_query():
+    expect = Q()
+    result = parse_query({})
+    eq_(expect, result)
 
 
 def test_single_query():
     expect = Q(a=1)
     result = parse_query({'a': 1})
-    compare_query(expect, result)
+    eq_(expect, result)
 
 
 def test_and_query():
     expect = Q(a=1, b=2)
     result = parse_query({'a': 1, 'b': 2})
-    compare_query(expect, result)
+    eq_(expect, result)
+
+
+def test_ordered_and_query():
+    """The order of arguments shouldn't matter."""
+    expect = Q(a=1, b=2)
+    result = parse_query({'b': 2, 'a': 1})
+    eq_(expect, result)
 
 
 def test_or_query():
     expect = Q(a=1) | Q(b=2)
     result = parse_query({'$or': [{'a': 1}, {'b': 2}]})
-    compare_query(expect, result)
+    eq_(expect, result)
 
 
 def test_and_or_query():
     expect = Q(a=1, b=2) | Q(c=3)
     input_ = {'$or': [{'a': 1, 'b':2}, {'c': 3}]}
     result = parse_query(input_)
-    compare_query(expect, result)
+    eq_(expect, result)
+
+
+def test_not_query():
+    expect = Q(a=1)
+    expect.negate()
+    result = parse_query({'$not': [{'a': 1}]})
+    eq_(expect, result)
+
+
+def test_or_not_query():
+    a = Q(a=1)
+    a.negate()
+    expect = Q(b=2) | a
+    result = parse_query({'$or': [{'$not': [{'a': 1}]}, {'b': 2}]})
+    eq_(expect, result)
+
+
+def test_gt_query():
+    expect = Q(a__gt=1)
+    result = parse_query({'a': {'$gt': 1}})
+    eq_(expect, result)
+
+
+def test_lt_query():
+    expect = Q(a__lt=3)
+    result = parse_query({'a': {'$lt': 3}})
+    eq_(expect, result)
+
+
+def test_gt_lt_query():
+    expect = Q(a__gt=3, a__lt=7)
+    result = parse_query({'a': {'$gt': 3, '$lt': 7}})
+    eq_(expect, result)
